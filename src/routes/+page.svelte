8b24f07f3ead497ebdfb7069f15fff6af2eec77b
Elias Luttinen <elias.luttinen@gmail.com>
2022-10-03 19:16:44 +0300

    card styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -59,18 +59,20 @@
 </script>
 
 <div class="w-full min-h-screen bg-gray-100 p-7">
-	<h1 class="font-bold text-center text-3xl mt-2 mb-4 md:text-5xl md:mt-5 md:mb-9">
-		<span class="text-red-600">Top 100</span> ruokablogit
-	</h1>
+	<div class="container mx-auto">
+		<h1 class="font-bold text-center text-3xl mt-2 mb-4 md:text-5xl md:mt-5 md:mb-9">
+			<span class="text-red-600">Top 100</span> ruokablogit
+		</h1>
 
-	<!-- cards -->
-	<div class="flex flex-wrap justify-center">
-		{#each items as item}
-			<div class="flex flex-col bg-white shadow-md m-4 sm:w-52">
-				<img class="object-cover sm:h-48 m-1" src={`/images/${item.img}`} alt="" />
-				<h2 class="text-center px-2 w-44">{item.title}</h2>
-				<!-- <a href={item.link}>Linkki</a> -->
-			</div>
-		{/each}
+		<!-- cards -->
+		<div class="flex flex-wrap justify-center">
+			{#each items as item}
+				<div class="flex flex-col bg-white shadow-md m-4 w-52">
+					<img class="object-cover m-1 h-52" src={`/images/${item.img}`} alt="" loading="lazy" />
+					<h2 class="text-center px-2 w-52">{item.title}</h2>
+					<!-- <a href={item.link}>Linkki</a> -->
+				</div>
+			{/each}
+		</div>
 	</div>
 </div>
